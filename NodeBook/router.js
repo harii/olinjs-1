d632ff1c3761ebf6c7ad28d5417593597e632f8f
harii <heyhari129@gmail.com>
2013-01-26 19:23:03 -0500

    made router say hello world

--- NodeBook/router.js
+++ NodeBook/router.js
@@ -8,6 +8,22 @@
 //  import it into server.js, where it'll be used with the rest of the server.
 //  I think that makes more sense.
 
+//var http = require("http");
 
 
+function router(response) {
+    
+    // alright, so the request comes in the form of a URL or button push.
+    // let's assume it's a URL for the time being.
+    // so assuming we get a request in the form of a URL, we need to parse that URL
+    // so for each url, we need to have a specific response.
+    
+    // now here we'll format the response from the server when it gets a request
+    response.writeHead(200, {"Content-Type": "text/plain"}); // gotta specify the format
+    response.write("Hello World"); // gotta specify what to say
+    response.end(); // gotta specify that we're done
+     
+}
+
+exports.route = router;
 
--- NodeBook/server.js
+++ NodeBook/server.js
@@ -1,21 +1,17 @@
 
 // "require" means import
 var http = require("http");
+var router = require("./router");
 
 // to turn this server into a module, like "http" above, we make a wrapper function
 //  called "start" and "export" the function
+
 function start() {
     // heads up, the argument to createServer is a function 
     //  we're quickly defining here on how to deal with requests and respond
     http.createServer(function(request, response) {
-        
         console.log("Request received.");
-        
-        // now here we'll format the response from the server when it gets a request
-        response.writeHead(200, {"Content-Type": "text/plain"}); // gotta specify the format
-        response.write("Hello World"); // gotta specify what to say
-        response.end(); // gotta specify that we're done
-        
+        router.route(response);
     }).listen(8888);
     console.log("Server has started.");
 }
